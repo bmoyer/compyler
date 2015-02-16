bc09ce037869edfdee932e68ea0b923a7ba3cfa6
Ben Moyer <bensmoyer@gmail.com>
2015-02-16 08:08:24 -0500

    end of tutor5, added break and more control structures

--- control.py
+++ control.py
@@ -114,18 +114,7 @@ def term():
             expected('MulOp')
 
 def expression():
-    if is_add_op(look):
-        print('CLR D0')
-    else:
-        term()
-    while is_add_op(look):
-        print('MOVE D0,-(SP)')
-        if look == '+':
-            add()
-        elif look == '-':
-            subtract()
-        else:
-            expected('AddOp')
+    print '<expr>'
 
 def new_label():
     global lcount
@@ -138,24 +127,24 @@ def post_label(l):
     print l + ':'
 
 def do_program():
-    block()
+    block('')
     if look != 'e':
         expected('end')
     print('END')
 
-def do_if():
+def do_if(l):
     match('i')
     condition()
     l1= new_label()
     l2 = l1
     print 'BEQ ' + l1
-    block()
+    block(l)
     if look == 'l':
         match('l')
         l2 = new_label()
         print 'BRA ' + l2
         post_label(l1)
-        block()
+        block(l)
     match('e')
     post_label(l2)
 
@@ -166,7 +155,7 @@ def do_while():
     post_label(l1)
     condition()
     print 'BEQ ' + l2
-    block()
+    block(l2)
     match('e')
     print 'BRA ' + l1
     post_label(l2)
@@ -174,26 +163,76 @@ def do_while():
 def do_loop():
     match('p')
     l1 = new_label()
+    l2 = new_label()
     post_label(l1)
-    block()
+    block(l2)
     match('e')
     print 'BRA ' + l1
+    post_label(l2)
 
 def do_repeat():
     match('r')
     l1 = new_label()
+    l2 = new_label()
     post_label(l1)
-    block()
+    block(l2)
     match('u')
     condition()
     print 'BEQ ' + l1
+    post_label(l2)
 
-def block():
+def do_for():
+    match('f')
+    l1 = new_label()
+    l2 = new_label()
+    name = get_name()
+    match('=')
+    expression()
+    print 'SUBQ #1,D0'
+    print 'LEA ' + name + '(PC),A0'
+    print 'MOVE D0,(A0)'
+    expression()
+    print 'MOVE D0,-(SP)'
+    post_label(l1);
+    print 'LEA ' + name + '(PC),A0'
+    print 'MOVE (A0),D0'
+    print 'ADDQ #1,D0'
+    print 'MOVE D0,(A0)'
+    print 'CMP (SP),D0'
+    print 'BGT ' + l2
+    block(l2)
+    match('e');
+    print 'BRA ' + l1
+    post_label(l2);
+    print 'ADDQ #2,SP'
+
+def do_do():
+    match('d')
+    l1 = new_label()
+    l2 = new_label()
+    expression()
+    print 'SUBQ #1,D0'
+    post_label(l1)
+    print 'MOVE D0,-(SP)'
+    block(l2)
+    print 'MOVE (SP)+,D0'
+    print 'DBRA D0,' + l1
+    print 'SUBQ #2,SP'
+    post_label(l2)
+    print 'ADDQ #2,SP'
+
+def do_break(l):
+    match('b')
+    if l != '':
+        print 'BRA ' + l
+    else:
+        abort('No loop to break from')
+
+def block(l):
     while not (look in ['e', 'l', 'u']):
-        #print "look was ", look
         old_look = look
         if old_look == 'i':
-            do_if()
+            do_if(l)
             continue
         elif old_look == 'w':
             do_while()
@@ -203,6 +242,15 @@ def block():
             continue
         elif old_look == 'r':
             do_repeat()
+            continue
+        elif old_look == 'f':
+            do_for()
+            continue
+        elif old_look == 'd':
+            do_do()
+            continue
+        elif old_look == 'b':
+            do_break(l)
             continue
         else: 
             other()
