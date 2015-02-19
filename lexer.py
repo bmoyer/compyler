93a8493c5a1eb77bcf112ad0a03d53e0e4abb30a
Ben Moyer <bensmoyer@gmail.com>
2015-02-18 20:49:43 -0500

    correctly tokenizing all inputs

--- lexer.py
+++ lexer.py
@@ -5,6 +5,7 @@ import sys
 class Symbol : str
 
 class SymType:
+    Null = -1
     IfSym = 0
     ElseSym = 1
     EndifSym = 2
@@ -13,24 +14,35 @@ class SymType:
     Number = 5
     Operator = 6
 
-sym_table = list()
+Symbols = [SymType.Null, SymType.IfSym, SymType.ElseSym, SymType.EndifSym, 
+            SymType.EndSym, SymType.Ident, SymType.Number, 
+            SymType.Operator]
 
-keywords = ['IF','ELSE','ENDIF','END']
+
+keywords = ['NULL','IF','ELSE','ENDIF','END']
 
 idx = 0
 look = ""
 token = -1
+value = ''
 
 def lookup(table, string):
+    try:
+        idx = keywords.index(string)
+    except:
+        idx = 0
+    return idx
+    '''
     found = False
     i = len(table) - 1
-    while(i > 0) and not found:
+    while(i > 0) and (not found):
         if string == table[i]:
+            print "FOUND TOK:"
             found = True
         else:
             i -= 1
     return i
-
+    '''
 
 def get_lines():
     stop = ''
@@ -103,6 +115,7 @@ def get_num():
 def get_name():
     x = ''
     if not look.isalpha():
+        print "LOOK WAS ",look
         expected('name')
     while look.isalnum():
         x += look.upper()
@@ -111,17 +124,29 @@ def get_name():
     return x
 
 def scan():
+    global value
+    global token
     if look.isalpha():
-        tok = get_name()
+        value = get_name()
+        k = lookup(Symbols, value)
+        if k == 0:
+            token = SymType.Ident
+        else:
+            token = Symbols[k-1]
     elif look.isdigit():
-        tok = get_num()
+        value = get_num()
+        token = SymType.Number
     elif is_op(look):
-        tok = get_op()
+        value = get_op()
+        token = SymType.Operator
+    elif look == ';':
+        get_char()
+        token = SymType.EndSym;
     else:
-        tok = look
+        value = look
+        token = SymType.Operator
         get_char()
     skip_white()
-    return tok
 
 def factor():
     if look == '(':
@@ -196,10 +221,20 @@ def assignment():
     print('LEA ' + name + '(PC),A0')
     print('MOVE D0,(A0)')
 
+# get code from user input
 stream = get_lines() 
+
 initialize()
 while True:
-    tok = scan()
-    print tok
-    if tok == ';':
+    scan()
+    if token == SymType.Ident:
+        print 'Ident '
+    elif token == SymType.Number:
+        print 'Number '
+    elif token == SymType.Operator:
+        print 'Operator '
+    elif token == SymType.EndSym:
         break
+    else:
+        print 'Keyword '
+    print value
